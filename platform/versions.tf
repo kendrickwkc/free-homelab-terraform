889415611eda1516bf647e291261ff623607f2a3
kc <kendrick.wongkc@gmail.com>
2026-08-24 15:33:44 +0800

    chore: upgrade OCI provider to v8

--- platform/versions.tf
+++ platform/versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     oci = {
       source  = "oracle/oci"
-      version = "~> 7.0"
+      version = "~> 8.0"
     }
   }
   # Remote state is configured via a gitignored backend.tfbackend file:
