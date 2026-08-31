fa971183c38982889ddd87c150c98a0a63cb79eb
kc <kendrick.wongkc@gmail.com>
2026-08-31 10:20:53 +0800

    Upgrade cluster to v1.34.2 (1.33 support lapsed Aug 10)

--- platform/variables.tf
+++ platform/variables.tf
@@ -23,7 +23,7 @@ variable "availability_domain" {
 variable "kubernetes_version" {
   description = "OKE Kubernetes version (from oci ce cluster create-options)"
   type        = string
-  default     = "v1.33.1"
+  default     = "v1.34.2"
 }
 
 variable "ssh_public_key" {
